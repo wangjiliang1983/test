90583f069160e4e56bcd62cd613b99d8ff291f3d
wangjiliang1983 <wangjiliang1983@126.com>
2019-02-09 07:31:13 +0800

    updated Chapter 3.2.2

---
+++ crashcourse/de03_02_motorcycles.py
@@ -0,0 +1,16 @@
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+motorcycles[0] = 'ducati'
+print(motorcycles)
+
+motorcycles.append('ducati')
+print(motorcycles)
+
+motorcycles = []
+motorcycles.append('honda')
+motorcycles.append('yamaha')
+motorcycles.append('suzuki')
+print(motorcycles)
+
+motorcycles.insert(0,'ducati')
+print(motorcycles)
