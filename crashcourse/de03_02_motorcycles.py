5ee5b68649cf0c4a8a50f58b4fb2952b1e6a4e0f
wangjiliang1983 <wangjiliang1983@126.com>
2019-02-09 13:55:14 +0800

    update  Chapter 3.2

--- crashcourse/de03_02_motorcycles.py
+++ crashcourse/de03_02_motorcycles.py
@@ -14,3 +14,36 @@ print(motorcycles)
 
 motorcycles.insert(0,'ducati')
 print(motorcycles)
+
+del motorcycles[0]
+print(motorcycles)
+del motorcycles[1]
+print(motorcycles)
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+poped_motorcycles = motorcycles.pop()
+print(motorcycles)
+print(poped_motorcycles)
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+last_owned = motorcycles.pop()
+print("The last motorcycle I owned was a " + last_owned.title() + ".")
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+first_owned = motorcycles.pop(0)
+print('The first motorcycle I owned was a ' + first_owned.title() + '.')
+
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+print(motorcycles)
+motorcycles.remove('ducati')
+print(motorcycles)
+
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+print(motorcycles)
+too_expensive = 'ducati'
+motorcycles.remove(too_expensive)
+print(motorcycles)
+print("\nA " + too_expensive.title() + " is too expensive for me.")
+
+
