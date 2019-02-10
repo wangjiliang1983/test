a4277ee7b30015d980bc0570084b9a0399cbae79
wangjiliang1983 <wangjiliang1983@126.com>
2019-02-11 07:24:15 +0800

    update Chapter 4

---
+++ crashcourse/ex04_13_buffe.py
@@ -0,0 +1,6 @@
+buffe_food = ('burger', 'chips', 'cola', 'chicken', 'soda')
+for food in buffe_food:
+    print(food)
+buffe_food = ('burger', 'coffee', 'chips','rice', 'chicken')
+for food in buffe_food:
+    print(food)
